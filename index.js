3dd2689fd390fc6c4c01360c567ec090dcadcc50
Mark F Guerra <markfguerra@gmail.com>
2016-12-17 15:12:24 -0500

    Add message indicating when a user connects via Socket.IO

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/webclient/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('A user connected via Socket.IO');
 });
 
 http.listen(3000, function(){
