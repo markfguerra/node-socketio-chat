ced146aac62f44f2982a3ada88ec66be120cd62f
Mark F Guerra <markfguerra@gmail.com>
2016-12-18 17:23:38 -0500

    Configure express to serve static content from the ‘webclient’ folder

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/webclient/index.html');
-});
+// Configure express to serve static content from the ‘webclient’ folder
+app.use(express.static('webclient'));
 
 io.on('connection', function(socket){
   console.log('A user connected via Socket.IO');
