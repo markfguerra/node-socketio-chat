2dda77562d72f7c8aa5b3b40339aea2405d7b3dc
Mark F Guerra <markfguerra@gmail.com>
2016-12-29 14:58:54 -0500

    Refactor <script> tag content into chatapi.js and gui.js

---
+++ webclient/js/chatapi.js
@@ -0,0 +1,32 @@
+var chatApi = (function() {
+	// Private members
+
+	// The socket that communicates with the server
+	var socket = io();
+
+	// Set up functions
+
+	// The event that fires when a message is received from the server
+	socket.on('chat message', function(messageText) {
+		// default listener
+		console.log("Chat received from server: " + messageText);
+	});
+
+	// Return value is the public API
+	return {
+		sendText : function(messageText) {
+			// Send message to the server.
+			// Sends an event called 'chat message' containing the messageText
+
+			// Instead of messageText, we could opt to send objects.
+			socket.emit('chat message', messageText);
+		},
+		// Sets the listener for receiving a chat message from the server
+		onMessageReceived : function(f) {
+			if (typeof(f) === "function")
+				socket.on('chat message', f);
+			else
+				console.log("Tried to call chatApi.onMessageReceived() without a function.");
+		}
+	};
+})();
---
+++ webclient/js/gui.js
@@ -0,0 +1,34 @@
+chatApi.onMessageReceived(function(messageText) {
+	// todo: learn why these types of operations are expensive and how to
+	// mitigate those expenses;
+	// Add a new message to the list of messages
+	$('#messages').append($('<li class="list-group-item">').text(messageText));
+
+	// Scroll the chat view to the bottom so the last message is shown,
+	// if the user is already scrolled to the bottom or is fairly close.
+	var $viewport = $('.viewport')[0];
+	var scrollTop = $viewport.scrollTop;
+	var scrollHeight = $viewport.scrollHeight;
+	var viewportHeight = $viewport.clientHeight;
+	var thresh = 100;
+	if(scrollHeight -  scrollTop - viewportHeight <= thresh) {
+		$viewport.scrollTop = scrollHeight;
+	}
+});
+
+// Listener for Send button
+$('form').submit(function(){
+	// Selector for the text input box
+	var inputBoxSel = '#message';
+
+	// Text from the input box
+	var messageText = $(inputBoxSel).val();
+	
+	chatApi.sendText(messageText);
+
+	// Clear messageText input box
+	$(inputBoxSel).val('');
+
+	// Prevent the submit event from reaching the default submit action
+	return false;
+});
